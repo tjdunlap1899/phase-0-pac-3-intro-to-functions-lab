486195e5a41ee611c0de84eac071adcd9d9d8bdb
Trevor Dunlap <tjdunlap1899@gmail.com>
2022-11-07 17:12:14 -0500

    commit tree 3c2c78d77f7f0ac8dc0291b39849ff1591633d45

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    // todo
+  return string;
+}
+"Hello!".toUpperCase(); // `HELLO!`
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    // todo
+  return string;
+}
+"Hello!".toLowerCase(); // `HELLO!`
+function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    // todo
+  return string;
+}
+"Hello!".toUpperCase(); // `HELLO!`
+function logShout(string) {
+    return string.toUpperCase();
+  }
+  const string = "Hello!";
+  console.log(string);
+  console.log(string.toUpperCase());
+  console.log(string === string.toUpperCase());
+
+
